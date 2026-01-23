b10f6b6d5ce0376f7052cf7415f9c4a5d9b2225f
cparmor350-source <cparmor350@alumnos.cei.es>
2026-01-23 09:25:04 +0100

    fiinish

--- Main.java
+++ Main.java
@@ -2,41 +2,40 @@ public class Main {
     public static void main(String[] args) {
         Tamagotchi miTamagotchi = new Tamagotchi();
         
-        System.out.println("=== BIENVENIDO A TU TAMAGOTCHI ===");
+        System.out.println("Bienvenidoo al tamagotchi");
         System.out.println();
-      
         System.out.println("Estado inicial:");
         miTamagotchi.showStats();
         System.out.println();
         
-        System.out.println("Jugando...");
+        System.out.println("Jugando");
         System.out.println("Estado: " + miTamagotchi.play());
         miTamagotchi.showStats();
         System.out.println();
         
-        System.out.println("Jugando otra vez...");
+        System.out.println("Jugando de nuevo");
         System.out.println("Estado: " + miTamagotchi.play());
         miTamagotchi.showStats();
         System.out.println();
         
-        System.out.println("Comiendo...");
+        System.out.println("Comiendo.");
         System.out.println("Estado: " + miTamagotchi.eat());
         miTamagotchi.showStats();
         System.out.println();
         
-        System.out.println("Jugando mucho más...");
+        System.out.println("Jugando ");
         for (int i = 0; i < 5; i++) {
             System.out.println("Estado: " + miTamagotchi.play());
         }
         miTamagotchi.showStats();
         System.out.println();
         
-        System.out.println("Durmiendo...");
+        System.out.println("Durmiendo");
         System.out.println("Estado: " + miTamagotchi.sleep());
         miTamagotchi.showStats();
         System.out.println();
         
-        System.out.println("Comiendo mucho...");
+        System.out.println("Comiendo bastante ");
         for (int i = 0; i < 3; i++) {
             System.out.println("Estado: " + miTamagotchi.eat());
         }
